b774e8fa688695e46e1843ba3ce98e053912d3c6
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2019-10-07 16:39:49 +0800

    Re-added nebular and removed Material. I also did some other stuff like removing new item and list components for a new approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,23 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
-import { NewitemComponent } from "./home/newitem/newitem.component";
-import { ListComponent } from "./home/list/list.component";
 import { ShoppinglistitemsService } from "./shoppinglistitems.service";
-import { MaterialModule } from "./material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule } from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { NebularModule } from "./nebular.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PagenotfoundComponent,
-    NewitemComponent,
-    ListComponent
-  ],
+  declarations: [AppComponent, HomeComponent, PagenotfoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    NbThemeModule.forRoot({ name: "default" }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NebularModule
   ],
   providers: [ShoppinglistitemsService],
   bootstrap: [AppComponent]
