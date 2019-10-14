91c4495c122ea10ddc9f026a17120be2703d206c
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2019-10-14 19:51:36 +0800

    just had to add firebase credentials to production environment file to actually deploy it

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBh7-A5Jfr4Kj2MVL2eyIB1CCEHAi33ENA",
+    authDomain: "shoppinglistapp-43.firebaseapp.com",
+    databaseURL: "https://shoppinglistapp-43.firebaseio.com",
+    projectId: "shoppinglistapp-43",
+    storageBucket: "shoppinglistapp-43.appspot.com",
+    messagingSenderId: "720682783602",
+    appId: "1:720682783602:web:ea500f49c872c5e2e843eb",
+    measurementId: "G-QTBKP7RH37"
+  }
 };
