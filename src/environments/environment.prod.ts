b65f2885b3335b7f24898a210a7f1bf53562ac28
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2020-02-09 15:05:23 +0800

    Removed firebase info I did this because I'm deleting the firebase project for this.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
+  // TODO: Fill the firebase info.
   firebase: {
-    apiKey: "AIzaSyBh7-A5Jfr4Kj2MVL2eyIB1CCEHAi33ENA",
-    authDomain: "shoppinglistapp-43.firebaseapp.com",
-    databaseURL: "https://shoppinglistapp-43.firebaseio.com",
-    projectId: "shoppinglistapp-43",
-    storageBucket: "shoppinglistapp-43.appspot.com",
-    messagingSenderId: "720682783602",
-    appId: "1:720682783602:web:ea500f49c872c5e2e843eb",
-    measurementId: "G-QTBKP7RH37"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
